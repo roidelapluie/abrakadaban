6b540c6d6eca3db97f540384761f622ddb871564
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-01 18:59:46 +0100

    add models representation

--- abrakadaban/kanban/models.py
+++ abrakadaban/kanban/models.py
@@ -11,18 +11,29 @@ class Idea(models.Model):
     user = models.ForeignKey(User, null=True)
     creation_date = models.DateTimeField(_("Creation date"))
 
+    def __unicode__(self):
+        return self.title
+
 class Comment(models.Model):
     idea =  models.ForeignKey(Idea)
     user = models.ForeignKey(User, null=True)
     text = models.TextField(_("Text"), blank=True)
     creation_date = models.DateTimeField(_("Creation date"))
 
+    def __unicode__(self):
+        return self.text
+
 class Workspace(models.Model):
     title = models.CharField(_("Title"), max_length=64)
     members = models.ManyToManyField(User, null=True)
     workflow = models.ManyToManyField("Workflow")
 
+    def __unicode__(self):
+        return self.title
+
 class Workflow(models.Model):
     order = models.IntegerField(_("Order"))
     title = models.CharField(_("Title"), max_length=16)
 
+    def __unicode__(self):
+        return '%s - %s' % (str(self.order), self.title)
