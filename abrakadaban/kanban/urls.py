bf70d22e73befb8aa7b0322aea9cd80cec1218dc
Laurent Peuch <cortex@worlddomination.be>
2013-03-01 23:00:37 +0100

    [fix] good path to index.haml template

--- abrakadaban/kanban/urls.py
+++ abrakadaban/kanban/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('kanban.views',
-    url(r'^$', TemplateView.as_view(template_name='kanban/index.haml'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='index.haml'), name='home'),
     url(r'^kanban.js$', TemplateView.as_view(template_name="kanban/kanban.js"), name='kanbanjs'),
     url(r'^workspace.json$', 'workspacesjson', name='workspacesjson'),
-    url(r'^workspaces$', TemplateView.as_view(template_name='kanban/index.haml'), name='workspacejson'),
+    url(r'^workspaces$', TemplateView.as_view(template_name='index.haml'), name='workspacejson'),
     url(r'^workspaces/(\d+)$', 'workspacejson'),
     url(r'^workspaces.html$', TemplateView.as_view(template_name='kanban/workspaces.haml'), name='workspacelist'),
     url(r'^workspace.html$', TemplateView.as_view(template_name='kanban/workspace.haml'), name='workspaceview'),
