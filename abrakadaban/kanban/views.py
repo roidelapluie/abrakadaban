a9892b1cb7504ff8dedaa1bbf0245c681ba8d2e2
Laurent Peuch <cortex@worlddomination.be>
2013-03-01 22:04:56 +0100

    use TemplateGenericView

--- abrakadaban/kanban/views.py
+++ abrakadaban/kanban/views.py
@@ -1,19 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from models import Workspace
 import json
-
-def home(request):
-    return render(request, 'kanban/index.haml')
-
-def workspacelist(request):
-    return render(request, 'kanban/workspaces.haml')
-
-def workspaceview(request):
-    return render(request, 'kanban/workspace.haml')
-
-def kanbanjs(request):
-    return render(request, 'kanban/kanban.js')
 
 def workspacejson(request):
     workspaces = Workspace.objects.all()
