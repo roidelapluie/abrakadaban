980fbb277158c3f329d94a1a927884f05d9d3b4e
Laurent Peuch <cortex@worlddomination.be>
2013-03-01 22:08:49 +0100

    fix, views getting string was bad

--- abrakadaban/kanban/urls.py
+++ abrakadaban/kanban/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 
-urlpatterns = patterns('views',
+urlpatterns = patterns('kanban.views',
     url(r'^$', TemplateView.as_view(template_name='kanban/index.haml'), name='home'),
     url(r'^kanban.js$', TemplateView.as_view(template_name="kanban/kanban.js"), name='kanbanjs'),
     url(r'^workspace.json$', 'workspacejson', name='workspacejson'),
