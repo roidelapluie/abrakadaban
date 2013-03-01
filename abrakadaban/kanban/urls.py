06464e6810a9880dbb981e06ba7e65e0fb9666aa
Laurent Peuch <cortex@worlddomination.be>
2013-03-01 22:05:52 +0100

    use string in urls to get views

--- abrakadaban/kanban/urls.py
+++ abrakadaban/kanban/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
-import views
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns('views',
     url(r'^$', TemplateView.as_view(template_name='kanban/index.haml'), name='home'),
     url(r'^kanban.js$', TemplateView.as_view(template_name="kanban/kanban.js"), name='kanbanjs'),
-    url(r'^workspace.json$', views.workspacejson, name='workspacejson'),
+    url(r'^workspace.json$', 'workspacejson', name='workspacejson'),
     url(r'^workspaces.html$', TemplateView.as_view(template_name='kanban/workspace.haml'), name='workspacelist'),
     url(r'^workspace.html$', TemplateView.as_view(template_name='kanban/workspace.haml'), name='workspaceview'),
 )
