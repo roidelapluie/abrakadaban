c1f507c4df7f401522a054fc331d3dbf64fc35fa
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-01 22:21:35 +0100

    allow view of workspace

--- abrakadaban/kanban/urls.py
+++ abrakadaban/kanban/urls.py
@@ -4,7 +4,9 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^kanban.js$', views.kanbanjs, name='kanbanjs'),
-    url(r'^workspace.json$', views.workspacejson, name='workspacejson'),
+    url(r'^workspace.json$', views.workspacesjson, name='workspacesjson'),
+    url(r'^workspaces$', views.home, name='workspacejson'),
+    url(r'^workspaces/(\d+)$', views.workspacejson),
     url(r'^workspaces.html$', views.workspacelist, name='workspacelist'),
     url(r'^workspace.html$', views.workspaceview, name='workspaceview'),
 )
