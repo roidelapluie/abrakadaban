bec3f8e033ac5708ff5c0e2523950e2dc27ce68b
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-01 19:22:41 +0100

    fix urls.py

--- abrakadaban/kanban/urls.py
+++ abrakadaban/kanban/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^kanban.js$', views.home, name='js'),
+    url(r'^kanban.js$', views.kanbanjs, name='kanbanjs'),
     url(r'^$', views.home, name='home'),
 )
