65fd01c188838569cad705b9eeb36d20d1338dd1
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-02 12:20:00 +0100

    add workflow in controllers

--- abrakadaban/kanban/static/js/controllers.js
+++ abrakadaban/kanban/static/js/controllers.js
@@ -2,11 +2,12 @@ function WorkspaceListCtrl($scope, Workspace){
     $scope.workspaces = Workspace.query();
 }
 
-function WorkspaceViewCtrl($scope, $routeParams, Workspace){
+function WorkspaceViewCtrl($scope, $routeParams, Workspace, Workflow){
     var workspace = Workspace.query({'workspaceId': $routeParams.workspaceId},
         function(){
             $scope.workspace = workspace[0];
             $scope.workspaceWidth = Math.floor(12/$scope.workspace.workflow.length);
+            $scope.workflows = Workflow.query({'workspaceId': $routeParams.workspaceId});
         }
     );
 
