c1b30bdba28c456c49bbeb0c66760f91d07e68e0
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-02 09:25:10 +0100

    install django rest framework

--- abrakadaban/abrakadaban/settings.py
+++ abrakadaban/abrakadaban/settings.py
@@ -121,9 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
+    'rest_framework',
     'kanban',
 )
 
