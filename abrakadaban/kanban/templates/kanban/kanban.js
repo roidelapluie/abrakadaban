7ba37b033e7c5552de4b061c0b2305d90cd467e6
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-02 14:02:03 +0100

    add user url inside js

--- abrakadaban/kanban/templates/kanban/kanban.js
+++ abrakadaban/kanban/templates/kanban/kanban.js
@@ -1,5 +1,6 @@
 var KanbanUrls = {};
 KanbanUrls['workspace_list'] = "{% url "kanban:workspace_list" %}";
 KanbanUrls['workflow_list'] = "{% url "kanban:workflow_list" %}";
+KanbanUrls['user_info'] = "{% url "kanban:user_info" %}";
 KanbanUrls['template_workspace_view'] = "{% url "kanban:template_workspace_view" %}";
 KanbanUrls['template_workspace_list'] = "{% url "kanban:template_workspace_list" %}";
