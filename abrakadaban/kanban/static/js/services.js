9c0b3535e4359729e60370c8c50ddc7ae5c5eee5
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-02 12:19:40 +0100

    add workflows to service

--- abrakadaban/kanban/static/js/services.js
+++ abrakadaban/kanban/static/js/services.js
@@ -1,8 +1,14 @@
 angular.module('KanbanServices', ['ngResource']).
     factory('Workspace', function($resource){
-  return $resource(KanbanUrls['workspace_list'] + '/:workspaceId', {}, {
-    query: {method:'GET', params:{workspaceId:'all'}, isArray:true},
-    get: {method:'GET', params:{workspaceId:'all'}, isArray:true}
-  });
+        return $resource(KanbanUrls['workspace_list'] + '/:workspaceId', {}, {
+            query: {method:'GET', params:{workspaceId:'all'}, isArray:true},
+              get: {method:'GET', params:{workspaceId:'all'}, isArray:true}
+        });
+}).
+    factory('Workflow', function($resource){
+        return $resource(KanbanUrls['workflow_list'] + '/:workspaceId', {}, {
+            query: {method:'GET', params:{workspaceId:'all'}, isArray:true},
+              get: {method:'GET', params:{workspaceId:'all'}, isArray:true}
+        });
 });
 
