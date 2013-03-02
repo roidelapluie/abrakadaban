018bca5e5c7cdefd4ef31bd21ea3ab153c0c3d0f
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-02 09:46:30 +0100

    add workflow_list url

--- abrakadaban/kanban/templates/kanban/kanban.js
+++ abrakadaban/kanban/templates/kanban/kanban.js
@@ -1,4 +1,5 @@
 var KanbanUrls = {};
 KanbanUrls['workspace_list'] = "{% url "kanban:workspace_list" %}";
+KanbanUrls['workflow_list'] = "{% url "kanban:workflow_list" %}";
 KanbanUrls['template_workspace_view'] = "{% url "kanban:template_workspace_view" %}";
 KanbanUrls['template_workspace_list'] = "{% url "kanban:template_workspace_list" %}";
