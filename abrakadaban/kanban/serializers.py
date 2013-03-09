09068ffe975286ba2f1e11d021eb041285d484e0
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-09 13:07:52 +0100

    autopep8 in serializers

--- abrakadaban/kanban/serializers.py
+++ abrakadaban/kanban/serializers.py
@@ -7,20 +7,23 @@ class WorkspaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workspace
 
+
 class WorkflowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workflow
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
 
+
 class IdeaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Idea
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username',)
-
