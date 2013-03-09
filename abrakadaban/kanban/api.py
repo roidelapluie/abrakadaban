9eb16b7655facb9334ede6e3476ef6e76b032904
Julien Pivotto <roidelapluie@inuits.eu>
2013-03-09 13:07:29 +0100

    autopep8 in api.py

--- abrakadaban/kanban/api.py
+++ abrakadaban/kanban/api.py
@@ -7,19 +7,24 @@ from tastypie.authorization import Authorization
 from django.utils import simplejson
 import datetime
 
+
 class UserAuthorization(Authorization):
     def read_list(self, object_list, bundle):
         return object_list.filter(username=bundle.request.user.username)
+
 
 class WorkspaceAuthorization(Authorization):
     def read_list(self, object_list, bundle):
         return object_list.filter(members__username=bundle.request.user.username)
 
+
 class IdeaAuthorization(Authorization):
     def read_detail(self, object_list, bundle):
         return True
+
     def update_detail(self, object_list, bundle):
         return True
+
 
 class UserResource(ModelResource):
     class Meta:
@@ -29,18 +34,23 @@ class UserResource(ModelResource):
         authentication = SessionAuthentication()
         authorization = UserAuthorization()
 
+
 class WorkflowResource(ModelResource):
     class Meta:
         queryset = Workflow.objects.all()
-        authorization= Authorization()
+        authorization = Authorization()
+
 
 class WorkspaceResource(ModelResource):
     workflow = fields.ToManyField(WorkflowResource, 'workflow', full=True)
     members = fields.ToManyField(UserResource, 'members', full=True)
-    idea = fields.ToManyField('kanban.api.IdeaResource', 'idea_set', related_name='workspace', full=True)
+    idea = fields.ToManyField(
+        'kanban.api.IdeaResource', 'idea_set', related_name='workspace', full=True)
+
     class Meta:
         authorization = WorkspaceAuthorization()
         queryset = Workspace.objects.all()
+
 
 class IdeaResource(ModelResource):
     user = fields.ForeignKey(UserResource, 'user', full=True)
@@ -51,15 +61,15 @@ class IdeaResource(ModelResource):
 
     class Meta:
         queryset = Idea.objects.all()
-        authorization= IdeaAuthorization()
+        authorization = IdeaAuthorization()
 
     def obj_create(self, bundle, request=None, **kwargs):
         json = simplejson.loads(bundle.request.body)
-        if json.has_key('workspace'):
+        if 'workspace' in json:
             kwargs['workspace'] = Workspace.objects.get(id=json['workspace'])
-        if json.has_key('workflow'):
+        if 'workflow' in json:
             kwargs['workflow'] = Workflow.objects.get(id=json['workflow'])
-        if json.has_key('title'):
+        if 'title' in json:
             kwargs['title'] = json['title']
         kwargs['creation_date'] = datetime.datetime.now()
         kwargs['user'] = User.objects.get(id=int(bundle.request.user.id))
@@ -68,9 +78,11 @@ class IdeaResource(ModelResource):
         bundle.obj.save()
         return bundle
 
+
 class CommentResource(ModelResource):
     idea = fields.ToOneField(IdeaResource, 'idea', full=True)
     user = fields.ToOneField(UserResource, 'user', full=True)
+
     class Meta:
         queryset = Comment.objects.all()
-        authorization= Authorization()
+        authorization = Authorization()
